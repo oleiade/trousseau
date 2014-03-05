b64a8ee8adff0621b2353c8c47f652d27d19fed1
Oleiade <tcrevon@gmail.com>
2014-03-05 08:26:27 +0100

    Hotfix homebrew formula

--- trousseau.rb
+++ trousseau.rb
@@ -2,15 +2,10 @@ require 'formula'
 
 class Trousseau < Formula
   homepage 'https://github.com/oleiade/trousseau'
-  url 'https://github.com/oleiade/trousseau/archive/0.2.4.tar.gz'
+  url 'https://github.com/oleiade/trousseau/releases/download/0.2.4/trousseau_0.2.4_darwin_amd64.zip'
   sha1 ''
 
-  depends_on 'go' => :build
-  depends_on 'mercurial' => :build
-  depends_on 'bzr' => :build
-
   def install
-    system 'make', 'all'
-    bin.install('bin/trousseau')
+    bin.install('trousseau')
   end
 end
