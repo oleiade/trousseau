587371d757b5282af4ddd7a102518e333e950066
Oleiade <tcrevon@gmail.com>
2014-09-10 19:12:16 +0200

    Update homebrew recipe to fetch 0.3.1 release

--- trousseau.rb
+++ trousseau.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Trousseau < Formula
   homepage 'https://github.com/oleiade/trousseau'
-  url 'https://github.com/oleiade/trousseau/releases/download/0.3.0/trousseau_0.3.0_darwin_amd64.zip'
+  url 'https://github.com/oleiade/trousseau/releases/download/0.3.1/trousseau_0.3.1_darwin_amd64.zip'
   sha1 ''
 
   def install
