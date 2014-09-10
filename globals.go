ddcd844fe8317ebd0db3279c0e15333a0a5e0ff9
Oleiade <tcrevon@gmail.com>
2014-09-10 19:27:49 +0200

    Remove unused ssh private keys path global

--- globals.go
+++ globals.go
@@ -2,7 +2,6 @@ package trousseau
 
 import (
 	"os"
-	"path/filepath"
 )
 
 // Global data store file path
@@ -13,11 +12,8 @@ func GetStorePath() string          { return gStorePath }
 // Gnupg trousseau master gpg key id
 var gMasterGpgId string = os.Getenv(ENV_MASTER_GPG_ID_KEY)
 
-// Ssh default identity file path
-var gPrivateRsaKeyPath string = filepath.Join(os.Getenv("HOME"), ".ssh", "id_rsa")
-
 // Keyring manager service and username to use in order to
 // retrieve trousseau main gpg key passphrase from system
 // keyring
-var gKeyringService string = os.Getenv(ENV_KEYRING_SERVICE_KEY)
+//var gKeyringService string = os.Getenv(ENV_KEYRING_SERVICE_KEY)
 var gKeyringUser string = os.Getenv(ENV_KEYRING_USER_KEY)
