55dec9a74d534e21cff1fb98a8aa68d243a765b5
Tamás Gulácsi <tgulacsi78@gmail.com>
2014-09-28 17:05:56 +0200

    crypto/openpgp/actions: fix Encrypt error returns

--- crypto.go
+++ crypto.go
@@ -2,10 +2,9 @@ package trousseau
 
 import (
 	"github.com/oleiade/trousseau/crypto/openpgp"
-	"path"
 	"os"
+	"path"
 )
-
 
 // Declare encryption types
 type CryptoType int
@@ -25,8 +24,8 @@ const (
 
 // Gnupg variables
 var GnupgHome = path.Join(os.Getenv("HOME"), ".gnupg")
-var GnupgPubring func()string = func()string { return path.Join(GnupgHome, "pubring.gpg") }
-var GnupgSecring func()string = func()string { return path.Join(GnupgHome, "secring.gpg") }
+var GnupgPubring func() string = func() string { return path.Join(GnupgHome, "pubring.gpg") }
+var GnupgSecring func() string = func() string { return path.Join(GnupgHome, "secring.gpg") }
 
 // DecryptAsymmetricPGP decrypts an OpenPGP message using GnuPG.
 func DecryptAsymmetricPGP(encryptedData []byte, passphrase string) ([]byte, error) {
