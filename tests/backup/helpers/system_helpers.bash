b13c620973a66db0f1f4df92d2caf21947f0bd59
Oleiade <the@softwarehater.com>
2015-01-31 12:32:15 +0100

    Remove legacy tests backup

--- tests/backup/helpers/system_helpers.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-# polite_sudo exposes a verbose and explicit sudo command
-# so any test who might need to ask for sudo password
-# would be more user-friendly.
-polite_sudo() {
-    sudo -p "Bats testing framework requires sudo access to setup the test key passphrase in keychain. Password: " "$@"
-}
