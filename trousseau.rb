c47d3ca4e631b0c6dd0040f91a235aa3a441f9d2
tcr <tcr@ableton.com>
2017-06-09 13:16:36 +0200

    Hotfix: homebrew recipe for version 4.0.0

--- trousseau.rb
+++ trousseau.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Trousseau < Formula
   homepage 'https://github.com/oleiade/trousseau'
-  url 'https://github.com/oleiade/trousseau/releases/download/0.3.4/trousseau_0.3.4_darwin_amd64.zip'
-  sha1 ''
+  url 'https://github.com/oleiade/trousseau/releases/download/0.4.0/trousseau_0.4.0_darwin_amd64.zip'
+  sha256 '9f4a794ad5427ef56c03dd1052c2f1cd236fe4655a7e88a841f67183ea12f00f'
 
   def install
     bin.install('trousseau')
